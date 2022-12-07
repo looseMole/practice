4da83e6abfd732350a65924f53baefd0c1618333
Thias Hyldig <62012079+looseMole@users.noreply.github.com>
2022-12-07 22:45:19 +0100

    First iteration of DayOne. Has obvious flaws.

--- Java/CodeKata/Kata02/DayOne.java
+++ Java/CodeKata/Kata02/DayOne.java
@@ -1,9 +1,42 @@
+import static java.util.Arrays.copyOfRange;
+
 public class DayOne extends Day{
+    int[] tableToSearch;
     public int chop(int target, int[] tableToSearch) {
+        int middleIndex;
+        this.tableToSearch = tableToSearch;
+
+        while(this.tableToSearch.length >= 1){
+            if(tableToSearch.length % 2 == 0) {
+                middleIndex = this.tableToSearch.length/2;
+            } else {
+                middleIndex = this.tableToSearch.length/2;
+                System.out.println(middleIndex);
+            }
 
 
-        else{
-            return -1;
+            if(this.tableToSearch.length == 1) {
+                if(this.tableToSearch[0] == target) { // TODO: Correct this. You cannot alter the length of the array, then return an index number of that array in a bin search.
+                    return 0; // The index of the only number left.
+                } else {
+                    return -1;
+                }
+            }
+
+            else if(this.tableToSearch[middleIndex] == target){
+                return middleIndex;
+            }
+
+            else if (this.tableToSearch[middleIndex] < target) {
+                this.tableToSearch = copyOfRange(this.tableToSearch, middleIndex + 1, this.tableToSearch.length);
+            }
+
+            else if (this.tableToSearch[middleIndex] > target) {
+                this.tableToSearch = copyOfRange(this.tableToSearch, 0, middleIndex);
+            }
+
         }
+
+        return -1; // To indicate the number could not be found.
     }
 }
